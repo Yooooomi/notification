dcbeb9dbb589d25f3cc4480ef606172eb24f7600
Timothee <timothee.boussus@gmail.com>
2023-05-07 17:16:38 +0200

    fixed front build

--- client/scripts/run/variables.sh
+++ client/scripts/run/variables.sh
@@ -11,14 +11,14 @@ fi
 
 cp "$VAR_PATH/variables-template.js" "$VAR_PATH/variables.js"
 
-if [ ! -z "$API_ENDPOINT" && ! -z "$PUBLIC_VAPID_KEY" ]
+if [[ ! -z "$API_ENDPOINT" && ! -z "$NOTIFICATION_PUBLIC_VAPID_KEY" ]]
 then
     echo "Setting API Endpoint to '$API_ENDPOINT'"
     sed -i "s;__API_ENDPOINT__;$API_ENDPOINT;g" "$VAR_PATH/variables.js"
 
-    echo "Setting public key to '$PUBLIC_VAPID_KEY'"
-    sed -i "s;__PUBLIC_VAPID_KEY__;$PUBLIC_VAPID_KEY;g" "$VAR_PATH/variables.js"
+    echo "Setting public key to '$NOTIFICATION_PUBLIC_VAPID_KEY'"
+    sed -i "s;__NOTIFICATION_PUBLIC_VAPID_KEY__;$NOTIFICATION_PUBLIC_VAPID_KEY;g" "$VAR_PATH/variables.js"
 else
-    echo "API_ENDPOINT is not defined, web app won't work"
+    echo "API_ENDPOINT or NOTIFICATION_PUBLIC_VAPID_KEY is not defined, web app won't work"
     exit 1
 fi
