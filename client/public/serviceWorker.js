b48e0866e72441c81eb09cf579a73b1bdf08e474
Timothee <timothee.boussus@gmail.com>
2023-05-07 22:05:56 +0200

    added logs of notification

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,5 +1,6 @@
 self.addEventListener("push", (event) => {
   let notification = event.data.json();
+  console.log('Incoming notification', notification);
   self.registration.showNotification(notification.title, {
     body: notification.content,
     icon: notification.icon,
